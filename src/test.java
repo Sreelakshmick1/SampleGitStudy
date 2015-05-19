3c1393aec13d667860a0e68ee2119f756f80a7b2
sreelakshmi@mindsandmachines.com <mindsandmachines@Sreelakhmis-Mac-book.local>
2015-05-19 22:48:42 +0100

    test

--- src/test.java
+++ src/test.java
@@ -3,12 +3,14 @@ public class test {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world");
+		/*System.out.println("Hello world");
 
 		
 		System.out.println("on the master");
 
-		System.out.println("few more prints");
+		System.out.println("few more prints");*/
+		
+		System.out.println("branch print");
 
 	}
 
